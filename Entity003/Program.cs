48d0c80ad00d5e3fa083affa002afd7837775b3a
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-09-24 14:04:00 +0300

    Section-B

--- Entity003/Program.cs
+++ Entity003/Program.cs
@@ -173,33 +173,140 @@ namespace Entity003
             //  to them
 
 
-            var aircraft = dbContext.Set<AireCraft>().ToList();
+            //var aircraft = dbContext.Set<AireCraft>().ToList();
 
-            foreach (var aircraftItem in aircraft) {
+            //foreach (var aircraftItem in aircraft) {
 
-                Console.WriteLine($"airecaftmodel = {aircraftItem.Model}");
+            //    Console.WriteLine($"airecaftmodel = {aircraftItem.Model}");
 
-                dbContext.Entry(aircraftItem)
-                     .Collection(a => a.AireCraftAssign).Load();
-                foreach (var assign in aircraftItem.AireCraftAssign)
-                {
-                    Console.WriteLine($"arrival {assign.Arrival} price = {assign.Price:C}  depature = {assign.Departure} duration = {assign.Duration}");
-                        
-                    dbContext.Entry(assign)
-                        .Reference(r=>r.Routes).Load();
+            //    dbContext.Entry(aircraftItem)
+            //         .Collection(a => a.AireCraftAssign).Load();
+            //    foreach (var assign in aircraftItem.AireCraftAssign)
+            //    {
+            //        Console.WriteLine($"arrival {assign.Arrival} price = {assign.Price:C}  depature = {assign.Departure} duration = {assign.Duration}");
 
-                    Console.WriteLine($"route = {assign.Routes.Origin}  distinaion ={assign.Routes.Distnation}  distance = {assign.Routes.Distnation}");
-               
-                }
+            //        dbContext.Entry(assign)
+            //            .Reference(r=>r.Routes).Load();
 
-                Console.WriteLine("-------------------------------------------------------------------");
-            
-            }
+            //        Console.WriteLine($"route = {assign.Routes.Origin}  distinaion ={assign.Routes.Distnation}  distance = {assign.Routes.Distnation}");
+
+            //    }
+
+            //    Console.WriteLine("-------------------------------------------------------------------");
+
+            //}
 
 
 
             #endregion
 
+
+
+            #region Section-B
+
+            //1.List all employees with their airline name.
+
+            //var airlineNameWithEmployee = dbContext.Set<AireLine>()
+            //    .GroupJoin(dbContext.Set<Employee>(),
+            //    aireline => aireline.AireLineId,
+            //    emp => emp.EmpId,
+            //    (aireline, employes) => new
+            //    {
+
+            //        AireLineName = aireline.AireLine_Name,
+            //        employes
+
+
+
+
+            //    });
+
+
+            //var airlineNameWithEmployee = from a in dbContext.Set<AireLine>()
+            //                              join e in (dbContext.Set<Employee>())
+            //                              on a.AireLineId equals e.EmpId into groups
+            //                              select new
+            //                              {
+            //                                  airlinename = a.AireLine_Name,
+            //                                  employees =groups
+
+
+            //                              };
+
+
+
+
+
+
+
+
+
+
+
+            //foreach (var airline in airlineNameWithEmployee) {
+
+            //    Console.WriteLine($"AireLine name ===> {airline.airlinename}");
+            //    foreach (var item in airline.employees)
+            //    {
+            //        Console.WriteLine($"Employess Name ={item.Name} ----- employeeID{item.EmpId} ");
+            //    }
+
+            //}
+
+
+            //            2.Show all routes with the aircraft model assigned and the
+            //airline name that owns the aircraft.
+
+            //var result = from a in dbContext.Set<AireLine>()
+            //             join c in dbContext.Set<AireCraft>()
+            //             on a.AireLineId equals c.AireLineOwnerId
+            //             join airrou in dbContext.Set<AireCraftRoute>()
+            //             on c.Id equals airrou.AireCraftId
+            //             join ro in dbContext.Set<Route>()
+            //          on airrou.RouteId equals ro.RouteId
+            //             select new
+            //             {
+
+            //                 AirlineName = a.AireLine_Name,
+            //                 AircraftModel = c.Model,
+            //                 AirCraftRouteId = airrou.AireCraft.Model,
+            //                 RouteName = ro.Origin
+
+            //             };
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine($"Airline: {item.AirlineName}, Aircraft: {item.AircraftModel}, AirCraftRouteID: {item.AirCraftRouteId}, Route: {item.RouteName}");
+            //}
+
+
+            //  3.For each airline, list its aircraft models.
+
+
+
+           // 4.Show all transactions(id, amount, description) along with the
+//airline name, but only where Amount > 20000.
+
+
+            //var result = from t in dbContext.Set<Transaction>()
+            //             join a in dbContext.Set<AireLine>()
+            //             on t.AireLineId equals a.AireLineId
+            //             where t.Amuont> 20000
+            //             select new
+            //             {
+            //                 TransactionId = t.TransactId,
+            //                 Amount = t.Amuont,
+            //                 Description = t.discription,
+            //                 AirlineName = a.AireLine_Name
+            //             };
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine($"TransactionID: {item.TransactionId}, Amount: {item.Amount}, Description: {item.Description}, Airline: {item.AirlineName}");
+            //}
+
+
+
+            #endregion
 
 
 
